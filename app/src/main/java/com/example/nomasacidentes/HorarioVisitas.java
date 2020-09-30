da77be7d40573f8615120f04296432da4ad7a3cb
SebastianAlcaino <72112886+SebastianAlcaino@users.noreply.github.com>
2020-09-30 05:33:06 -0300

    Merge branch 'master' of C:\Users\Sebas\AndroidStudioProjects\NomasAcidentes2 with conflicts.

---
+++ app/src/main/java/com/example/nomasacidentes/HorarioVisitas.java
@@ -0,0 +1,14 @@
+package com.example.nomasacidentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorarioVisitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_visitas);
+    }
+}
