76f1604e6d813732f7dde6bde5683ab01955a5a7
Shosukeke <deadlordkael@gmail.com>
2020-09-30 01:10:52 -0300

    asd

--- app/src/main/java/com/example/nomasacidentes/GestionVista.java
+++ app/src/main/java/com/example/nomasacidentes/GestionVista.java
@@ -1,4 +1,21 @@
 package com.example.nomasacidentes;
 
-public class GestionVista {
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GestionVista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vista_gestion);
+
+
+            }
+        }
+
+
--- app/src/main/java/com/example/nomasacidentes/MainActivity.java
+++ app/src/main/java/com/example/nomasacidentes/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import java.sql.*;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,13 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button   btn_ingresar=(Button)findViewById(R.id.btn_ingresar);
+        Button btn_ingresar=(Button)findViewById(R.id.btn_ingresar);
         btn_ingresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               /* try {
+                    ConnectOra conn =new ConnectOra("10.0.2.2","orcl","Portafolio","SEBA","qwerty");
+
+                }catch (Exception e){}*/
                 Intent login=new Intent(MainActivity.this,GestionVista.class);
                 MainActivity.this.startActivity(login);
                 finish();
+
             }
         });
     }
