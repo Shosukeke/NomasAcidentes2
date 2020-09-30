3ffcd1ebf52f05270e850c86b91413b3616614bf
tomaasay <43545653+tomaasay@users.noreply.github.com>
2020-09-30 16:37:28 -0300

    Se añadieron botones

--- app/src/main/java/com/example/nomasacidentes/DatosEmpresa.java
+++ app/src/main/java/com/example/nomasacidentes/DatosEmpresa.java
@@ -3,12 +3,23 @@ package com.example.nomasacidentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DatosEmpresa extends AppCompatActivity {
+Button btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_empresa);
+        btnCancelar = findViewById(R.id.btnCancelar1);
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nomasacidentes/SolicitudServicios.java
+++ app/src/main/java/com/example/nomasacidentes/SolicitudServicios.java
@@ -3,10 +3,14 @@ package com.example.nomasacidentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class SolicitudServicios extends AppCompatActivity {
+
+    Button btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,15 @@ public class SolicitudServicios extends AppCompatActivity {
 
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.combo_servicios, android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+
+        btnCancelar = findViewById(R.id.btnCancelar1);
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
