d83c90ba97785f3a950dfb8f522b141d15885c24
SebastianAlcaino <72112886+SebastianAlcaino@users.noreply.github.com>
2020-09-29 22:24:25 -0300

    PruebaGit 1

---
+++ app/src/main/java/com/example/nomasacidentes/PruebaGit.java
@@ -0,0 +1,14 @@
+package com.example.nomasacidentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PruebaGit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_git);
+    }
+}
